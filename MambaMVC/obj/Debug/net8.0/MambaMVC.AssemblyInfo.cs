81e472f63666456fa910fbeef6c16171a0fc2401
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2025-01-14 23:28:09 +0400

    slm

--- MambaMVC/obj/Debug/net8.0/MambaMVC.AssemblyInfo.cs
+++ MambaMVC/obj/Debug/net8.0/MambaMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MambaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd72e399550d7ebb93fe89fe13419ab4bbfc8907")]
 [assembly: System.Reflection.AssemblyProductAttribute("MambaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MambaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
